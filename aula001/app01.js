de5912479b898b518f10ab2b24ff80c16a586ed5
milenards <contatomillenarodrigues20@gmail.com>
2022-09-16 21:30:42 -0300

    Aula 01 javascript

---
+++ aula001/app01.js
@@ -0,0 +1,25 @@
+console.log("Olá, mundo")
+
+//variaveis
+nome = "millena"
+console.log(nome)
+
+var nome = "millena jesus"
+console.log(nome)
+
+let nome1 = "millena rodrigues"
+console.log(nome1)
+
+//constantes
+const sobrenome = "rodrigues"
+console.log(sobrenome)
+sobrenome = "jesus"
+
+//vamos usar
+let email = "contatomillenarodrigues20@gmail.com"
+const empresa = "Senac"
+
+let n1 = 20
+let n2 = 10
+
+console.log(n1+n2)

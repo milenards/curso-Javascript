a83004398c304c1284e57656e80a7bb192efe456
milenards <contatomillenarodrigues20@gmail.com>
2022-09-22 08:12:39 -0300

    Exercicio 005
    exercicio de estrutura de decisão com javascript

---
+++ exercicios/estrutura-decisao/005.js
@@ -0,0 +1,16 @@
+// //Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+// A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+// A mensagem "Reprovado", se a média for menor do que sete;
+// A mensagem "Aprovado com Distinção", se a média for igual a dez.
+
+let notas = 10
+
+if(notas >=7 && notas < 10){
+    console.log("Aprovado")
+}
+else if(notas < 7){
+    console.log("Reprovado")
+}
+else if (notas === 10){
+    console.log("Aprovado com Distinção")
+}

d9d0bfa10835731f7c173b68bd5ed395ec2da494
milenards <contatomillenarodrigues20@gmail.com>
2022-09-21 22:43:10 -0300

    exercicio 3 e 4
    corrigindo o exercicio 3 e fazendo o exercicio 4

--- exercicios/estrutura-decisao/003.js
+++ exercicios/estrutura-decisao/003.js
@@ -1,12 +1,12 @@
 //Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 
-let Sexo="M"
+let sexo="F"
 
 
-if(masculino == 'M'){
+if(sexo == 'M'){
     console.log("Masculino")
 }
-else if(feminino == 'F'){
+else if(sexo == 'F'){
     console.log("Feminino")
 }
 else{
---
+++ exercicios/estrutura-decisao/004.js
@@ -0,0 +1,12 @@
+//Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+let letra = 'z'
+
+if (letra == 'a' || letra =='e' || letra == 'i' || letra == 'o' || letra == 'u'){
+    console.log("Essa é vogal")
+}
+else if (letra == 'b' || letra == 'c' || letra == 'd' || letra == 'f' || letra == 'g' || letra == 'h' || letra == 'j' || letra == 'k' || letra == 'l' || letra == 'm' || letra == 'n' || letra == 'p' || letra == 'q' || letra == 'r' || letra == 's' || letra == 't' || letra == 'u' || letra == 'v' || letra == 'w' || letra == 'x' || letra == 'y' || letra == 'z') {
+    console.log("Essa é consoante")
+}
+
+

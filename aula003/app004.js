7e49d56532bf375a3f422c8585387ae62a5d41ae
milenards <contatomillenarodrigues20@gmail.com>
2022-09-23 22:13:07 -0300

    atividade 4 Pokemon

--- aula003/app004.js
+++ aula003/app004.js
@@ -1,9 +1,22 @@
 let pokemons = ["pikachu", "charmander", "bulbasouro", "squirtle"]
 
-let pokemonsFoto = [
-    "foto do pikacu",
+let pokemonsNome = [
+    "foto do pikachu",
     "foto do charmander",
     "foto do bulbasouro",
     "foto do squirtle"
-    
+
 ]
+let pokemonsFoto = [
+    "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
+    "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+    "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
+    "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png"
+]
+for (let i = 0; i < pokemonsNome.length; i++){
+    document.write("<div class = 'umpokemon'>")
+    document.write(`<h3>${pokemonsNome[i]}</h3>`)
+    document.write(`<img src = "${pokemonsFoto[i]}" alt="${pokemonsNome[i]}">`)
+    document.write("</div>")
+}
+
